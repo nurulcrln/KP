ebebe6e64ea7fdd1c4f0b4f3ce32242ec943fddb
Refian Aulia Perdana <78913508+refianP13@users.noreply.github.com>
2021-07-31 18:37:25 +0700

    create penjualan

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Penjualan;
 
-class PenjualanController extends Controller
+class penjualanController extends Controller
 {
     /**
      * Display a listing of the resource.
